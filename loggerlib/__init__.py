6f512ba47c1e8d79d184d054a54b762acdc0b631
Ivan <i.lutkin@foxford.ru>
2021-05-10 13:14:23 +0300

    added console logging

--- loggerlib/__init__.py
+++ loggerlib/__init__.py
@@ -8,23 +8,32 @@ import pymysql
 class DatabaseFileLogger:
     __str = ''
     __file = ''
-    __service = ''
+    __console_log = True
+    __file_log = False
+    __database_log = False
+    __service = 'example'
 
     def __init__(self, directory):
-        self.__config = configparser.ConfigParser()
-        self.__config.read(directory)
-        self.__service = self.__config['service']['name']
-        self.__file = os.path.join(self.__config['file']['directory'], self.__config['file']['filename'])
-        self.__conn = pymysql.connect(
-            host=self.__config['database']['host'],
-            port=int(self.__config['database']['port']),
-            user=self.__config['database']['user'],
-            password=self.__config['database']['password'],
-            database=self.__config['database']['database'],
-            cursorclass=pymysql.cursors.DictCursor
-        )
-        self.__cursor = self.__conn.cursor()
-        self.__f = open(self.__file, 'a')
+        if directory != '':
+            self.__config = configparser.ConfigParser()
+            self.__config.read(directory)
+            self.__console_log = bool(self.__config['console']['bool'])
+            self.__file_log = bool(self.__config['file']['bool'])
+            self.__database_log = bool(self.__config['database']['bool'])
+            self.__service = self.__config['service']['name']
+            if self.__file_log:
+                self.__file = os.path.join(self.__config['file']['directory'], self.__config['file']['filename'])
+                self.__f = open(self.__file, 'a')
+            if self.__database_log:
+                self.__conn = pymysql.connect(
+                    host=self.__config['database']['host'],
+                    port=int(self.__config['database']['port']),
+                    user=self.__config['database']['user'],
+                    password=self.__config['database']['password'],
+                    database=self.__config['database']['database'],
+                    cursorclass=pymysql.cursors.DictCursor
+                )
+                self.__cursor = self.__conn.cursor()
 
     def __db_write(self, level, message, time):
         logg = [(time, message, self.__service, level)]
@@ -36,27 +45,50 @@ class DatabaseFileLogger:
         self.__str = str(time) + ' | ' + message + ' | ' + self.__service + ' | ' + level + '\n'
         self.__f.write(self.__str)
 
+    def __console_write(self, level, message, time):
+        print(f'{time} | {message} | {self.__service} | {level}')
+
     def debug(self, message):
         time = datetime.datetime.now()
-        self.__db_write('DEBUG', message, time)
-        self.__file_write('DEBUG', message, time)
+        if self.__console_log:
+            self.__console_write('DEBUG', message, time)
+        if self.__database_log:
+            self.__db_write('DEBUG', message, time)
+        if self.__file_log:
+            self.__file_write('DEBUG', message, time)
 
     def info(self, message):
         time = datetime.datetime.now()
-        self.__db_write('INFO', message, time)
-        self.__file_write('INFO', message, time)
+        if self.__console_log:
+            self.__console_write('INFO', message, time)
+        if self.__database_log:
+            self.__db_write('INFO', message, time)
+        if self.__file_log:
+            self.__file_write('INFO', message, time)
 
     def warning(self, message):
         time = datetime.datetime.now()
-        self.__db_write('WARNING', message, time)
-        self.__file_write('WARNING', message, time)
+        if self.__console_log:
+            self.__console_write('WARNING', message, time)
+        if self.__database_log:
+            self.__db_write('WARNING', message, time)
+        if self.__file_log:
+            self.__file_write('WARNING', message, time)
 
     def error(self, message):
         time = datetime.datetime.now()
-        self.__db_write('ERROR', message, time)
-        self.__file_write('ERROR', message, time)
+        if self.__console_log:
+            self.__console_write('ERROR', message, time)
+        if self.__database_log:
+            self.__db_write('ERROR', message, time)
+        if self.__file_log:
+            self.__file_write('ERROR', message, time)
 
     def critical(self, message):
         time = datetime.datetime.now()
-        self.__db_write('CRITICAL', message, time)
-        self.__file_write('CRITICAL', message, time)
+        if self.__console_log:
+            self.__console_write('CRITICAL', message, time)
+        if self.__database_log:
+            self.__db_write('CRITICAL', message, time)
+        if self.__file_log:
+            self.__file_write('CRITICAL', message, time)
