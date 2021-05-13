5bee546de65bb507c86de4bf4817d3934ba22c59
Gleb Sakharov <35118149+skhrvg@users.noreply.github.com>
2021-05-13 13:39:12 +0300

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='pchelog',
     packages=find_packages(),
-    version='0.2',
+    version='0.3',
     description='Библиотека с возможностью логирования в MySQL БД и в файл',
     url='https://github.com/pchely/pchelog',
     author='ivanlut',
