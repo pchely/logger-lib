786172d73131facb93df1a1f0b808d6b53c70cbd
Gleb Sakharov <35118149+skhrvg@users.noreply.github.com>
2021-05-13 14:00:26 +0300

    hotfix v0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='pchelog',
     packages=find_packages(),
-    version='0.3',
+    version='0.4',
     description='Библиотека с возможностью логирования в MySQL БД и в файл',
     url='https://github.com/pchely/pchelog',
     author='ivanlut',
